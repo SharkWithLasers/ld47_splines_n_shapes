7370969556ce3f967e89a4cea1a390856fb51d53
unknown <michaeljkamm@gmail.com>
2020-10-03 00:32:56 -0700

    bspline probably works besides some borked shit. back to unity 2019.4.4

---
+++ Assets/Code/Controls/BSpline/BSplinePointGenerator.cs
@@ -0,0 +1,43 @@
+﻿using ScriptableObjectArchitecture;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BSplinePointGenerator : MonoBehaviour
+{
+    [SerializeField] private int numControlPoints = 5;
+    [SerializeField] private float initCircleSize = 2;
+
+    [SerializeField] private BSplinePoint bSplinePointPrefab;
+
+    [SerializeField] private GameEvent bSplinePointsGeneratedEvent; 
+
+    // yeah its mutable i know
+    public List<BSplinePoint> bSplinePoints { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bSplinePoints = new List<BSplinePoint>();
+        for (var i = 0; i < numControlPoints; i++)
+        {
+            var bspGO = Instantiate(bSplinePointPrefab);
+
+            bspGO.SetIndex(i);
+
+            var t = (((float)i) / numControlPoints) * Mathf.PI * 2;
+            bspGO.transform.position = new Vector2(
+                initCircleSize * Mathf.Cos(t),
+                initCircleSize * Mathf.Sin(t));
+
+            bSplinePoints.Add(bspGO);
+        }
+        bSplinePointsGeneratedEvent.Raise();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
