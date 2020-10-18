6702e62048776b96d8e603781a8c11d7dcc21a17
unknown <michaeljkamm@gmail.com>
2020-10-17 18:32:31 -0700

    camera event mask fix

---
+++ Assets/Code/Camera/CameraEventMask.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Camera))]
+public class CameraEventMask : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask cameraEventMask;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetComponent<Camera>().eventMask = cameraEventMask;
+    }
+}
